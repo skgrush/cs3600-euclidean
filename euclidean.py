0bb11a44767ba77fd7cfd1b8fea00e37cb6a80e4
Samuel K. Grush <samuel@grush.org>
2016-10-30 17:18:02 -0500

    Implemented project 2

--- euclidean.py
+++ euclidean.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+"""Find two numbers' GCD and its linear combination.
+usage: euclidean.py [a b | INPUT OUTPUT]
+
+ Pass no arguments for interactive mode.
+ Pass two integer arguments 'a' and 'b' to see their GCD and linear combination.
+ pass two path arguments 'INPUT' and 'OUTPUT' to read 'a' and 'b' from a file.
+"""
 
 import sys
 
@@ -39,16 +46,45 @@ def extendedEuclidean( a, b ):
 
 if __name__ == "__main__":
     
+    if '-h' in sys.argv or '--help' in sys.argv:
+        print(__doc__)
+        exit(0)
+    
+    
+    inputfile, outputfile = '',''
     a, b = 0, 0
     
-    # if there are two args, use them for a and b
-    # else take input
-    if len(sys.argv) == 3:
+    # if there are two args and they're ints, use them for a and b
+    if len(sys.argv) == 3 and sys.argv[1].isdigit() and sys.argv[2].isdigit():
         a, b = sys.argv[1:3]
-        
+    
     else:
-        a = input("Enter an integer value for a: ")
-        b = input("Enter an integer value for b: ")
+        # if there are two args, assume they're filenames
+        # else get them from input
+        if len(sys.argv) == 3:
+            inputfile, outputfile = sys.argv[1:3]
+        
+        else:
+            inputfile = input("Enter the name of the input file that " \
+                              "contains a and b: ")
+            outputfile= input("Enter the name of the output file to store " \
+                              "the GCD of a and b, and the linear " \
+                              "combination of GCD(a,b): ")
+        
+        # read from file
+        try:
+            with open(inputfile, 'r') as ifl:
+                a = ifl.readline().strip()
+                b = ifl.readline().strip()
+                    
+        except Exception as err:
+            print(type(err),err)
+            exit(-1)
+        
+        if not a or not b:
+            print("\nERROR: Not enough arguments in", repr(inputfile))
+            exit(1)
+        
     
     # Integer-ize a and b, watch for exceptions
     try:
@@ -58,9 +94,24 @@ if __name__ == "__main__":
         print("\nERROR: Arguments should be integers.")
         exit(1)
     
-    # Output the results
-    print("The GCD of a and b is:", euclideanGCD(a, b))
-    print("The linear combination of GCD(a,b) is: " \
-          "{2[0]} = {0}*{2[1]} + {1}*{2[2]}".format( a, b, 
-                                    extendedEuclidean(a, b))
-         )
+    
+    if outputfile:
+        
+        gcd_and_coefficients = extendedEuclidean(a, b)
+        
+        # Write the results to a file
+        with open(outputfile, 'w') as ofl:
+            
+            for i in gcd_and_coefficients:
+                ofl.write( str(abs(i)) )
+                ofl.write( '\n' )
+        
+        print("Output written to",repr(outputfile))
+    
+    else:
+        # Output the results
+        print("The GCD of a and b is:", euclideanGCD(a, b))
+        print("The linear combination of GCD(a,b) is: " \
+              "{2[0]} = {0}*{2[1]} + {1}*{2[2]}".format( a, b, 
+                                        extendedEuclidean(a, b))
+             )
